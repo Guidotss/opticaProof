4730911602facef811ac4ce09a437be8aefda8b3
Guidotss <guidoolguin@hotmail.com>
2022-10-21 18:59:20 -0300

    agregando admin pages

--- client/src/glasses/pages/HomePage.jsx
+++ client/src/glasses/pages/HomePage.jsx
@@ -10,7 +10,7 @@ export const HomePage = () => {
   const navigate = useNavigate();
 
   const navigateToSunGlasses = () => {
-    navigate('/lentesdesol');
+    navigate('/optica/lentesdesol');
   }
 
   return (
