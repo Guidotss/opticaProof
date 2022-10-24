047147397c672adbc4f73cbc8d5806f059d584ca
Guidotss <100441914+Guidotss@users.noreply.github.com>
2022-10-24 10:31:17 -0300

    empezando media querys del formulario admin page

--- client/src/glasses/admin/pages/NewGlassesAdminPage.jsx
+++ client/src/glasses/admin/pages/NewGlassesAdminPage.jsx
@@ -18,10 +18,10 @@ export const NewGlassesAdminPage = () => {
         sx={{
               padding:2, 
               border:'1px solid',
-              boxShadow:4, marginTop:'50px', 
+              boxShadow:4, marginTop:'5vh', 
               width:'50vw',
               height:'70vh', 
-              marginLeft:'600px', 
+              marginLeft:'35vw', 
               borderRadius:'15px', 
               backgroundColor:'#f6fceb'
             }}
@@ -31,32 +31,36 @@ export const NewGlassesAdminPage = () => {
       >
         <Grid item sx={{textAlign:'center'}}>
           <form>
-            <Grid container spacing={2} sx={{padding:2}} direction="column" alignItems="center" justifyContent="center">
-              <Grid item sx={{textAlign:'center'}}>
+            <Grid  container spacing={2} sx={{padding:2}} direction="column" alignItems="center" justifyContent="center">
+              <Grid item xs={12} sx={{textAlign:'center'}}>
                 <TextField
                   label="Nombre del anteojo"
                   variant="outlined"
-                  sx={{width:'500px'}}
+                  color="success"
+                  sx={{width:'500px','@media (max-width: 600px)':{width:'300px'}}}
                 />
               </Grid>
 
-              <Grid item sx={{textAlign:'center'}}>
+              <Grid item xs={12} sx={{textAlign:'center'}}>
                 <TextField
                   label="Modelo"
                   variant="outlined"
+                  color="success"
                   sx={{width:'500px'}}
                 />
               </Grid>  
 
               <Grid item  sx={{textAlign:'center'}}>
                 <TextField
+                  xs={12}
                   label="Marca"
                   variant="outlined"
+                  color="success"
                   sx={{width:'500px'}}
                 />
               </Grid>
 
-              <Grid item  sx={{textAlign:'center'}}>
+              <Grid item xs={12} sx={{textAlign:'center', }}>
                 <input
                   accept="image/*"
                   style={{ display: 'none' }}
@@ -66,8 +70,8 @@ export const NewGlassesAdminPage = () => {
                 />
 
                 <label htmlFor="raised-button-file">
-                  <Box sx={{width:'500px', height:'100px', border:'solid 1px gray',boxShadow:2,backgroundColor:'#14213d', borderRadius:'10px', marginTop:'20px'}}>
-                    <IconButton component="span">
+                  <Box xs={12} sx={{width:'500px', height:'100px', border:'solid 1px gray',boxShadow:2,backgroundColor:'#14213d', borderRadius:'10px', marginTop:'20px'}}>
+                    <IconButton xs={12} component="span">
                       <AddPhotoAlternate sx={{fontSize:'100px', marginTop:'-10px', color:'white'}}/>
                       <Typography sx={{color:'white', fontSize:'20px',fontWeight:'bold', marginTop:'-10px', marginLeft:'-10px'}}>Agregar imagen</Typography>
                     </IconButton>
@@ -80,6 +84,7 @@ export const NewGlassesAdminPage = () => {
             <Divider/>
             <Box sx={{textAlign:'center', marginTop:'20px'}}>
               <IconButton
+                xs={12}
                 type='submit' 
                 variant='contained'
                 sx={{
@@ -90,6 +95,7 @@ export const NewGlassesAdminPage = () => {
                   color:'white', 
                   fontWeight:'bold', 
                   fontSize:'20px',
+                  marginBottom:'20px',
                   '&:hover': {
                     backgroundColor: '#14213d',
                     opacity: [0.9, 0.8, 0.7],
@@ -97,7 +103,7 @@ export const NewGlassesAdminPage = () => {
                 }}
                 >
                   <Save sx={{fontSize:'30px', marginRight:'10px'}}/>
-                <Typography sx={{fontSize:'20px', fontWeight:'bold'}}>Guardar</Typography>
+                <Typography xs={12} sx={{fontSize:'20px', fontWeight:'bold'}}>Guardar</Typography>
               </IconButton>
             </Box>
           </form>
